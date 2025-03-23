dffa8504da1c2cd39ff2d53e747ef6d7ebd7870d
JuanpeDeveloper <juantandil123@gmail.com>
2025-03-23 20:05:31 -0300

    trabajo introduccion finalizado

--- practico-introduccion-a-cpp/ejercicio8.cpp
+++ practico-introduccion-a-cpp/ejercicio8.cpp
@@ -7,15 +7,45 @@ sí, separados por un espacio. Por ejemplo:
 10 2 3 1 7
 Salida del programa: los valores, separados por un espacio, en el orden inverso al ingresado.
 7 1 3 2 10
-
-
 */
 
 #include <iostream>
+using std::cin;
 using std::cout;
 using std::endl;
 
+void mostrarArreglo(int arreglo[], int tamaño)
+{
+  for (int i = 0; i < tamaño; i++)
+  {
+    cout << arreglo[i] << " ";
+  }
+  cout << "" << endl;
+}
+
+void mostrarArregloInvertido(int arreglo[], int tamaño)
+{
+  for (int i = tamaño - 1; i >= 0; i--)
+  {
+    cout << arreglo[i] << " ";
+  }
+  cout << "" << endl;
+}
+
 int main()
 {
+  int N;
+  cout << "Ingrese el tamanio del arreglo: ";
+  cin >> N;
+  int arreglo[N];
+  for (int i = 0; i < N; i++)
+  {
+    int num;
+    cout << "ingrese un numero: ";
+    cin >> num;
+    arreglo[i] = num;
+  }
+  mostrarArreglo(arreglo, N);
+  mostrarArregloInvertido(arreglo, N);
   return 0;
 }
